d27283dd7fecbfdf7c711b52efc5e1660a663690
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2023-11-19 18:30:04 +0100

    serverjs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
